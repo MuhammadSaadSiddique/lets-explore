a5c516e12764ff18f9b6b2cda18f1af7e169d79e
Muhammad Saad Siddique <26270646+MuhammaadSaad@users.noreply.github.com>
2022-10-16 01:02:48 +0500

    modify

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authRouter from "./routes/auth.js"; 
 import roomRouter from "./routes/room.js";
+import apiRouter from "./routes/api.js"; 
 import hotalRouter from "./routes/hotal.js";
 import usersRouter from "./routes/users.js";
 import cookieParser from 'cookie-parser';
-
-
+import path from 'path';
 
 const app = express();
+// const path = require("path")
 dotenv.config();
 
 const connect = async() =>{
@@ -26,7 +27,13 @@ app.use(express.json());
 app.use("/auth" , authRouter);
 app.use("/users" , usersRouter);
 app.use("/hotal" , hotalRouter);
-app.use("/room" , roomRouter);
+app.use("/room", roomRouter);
+app.use("/api", apiRouter);
+
+
+// app.use(express.static(path.join(__dirname, '../client/build')));
+
+
 
 app.use((err, req, res, next) => {
     const errorStatus = err.status || 500;
@@ -38,7 +45,8 @@ app.use((err, req, res, next) => {
       stack: err.stack,
     });
   });
-app.listen(8800 , ()=>{
+
+  app.listen(8800 , ()=>{
     connect();
     console.log('server  is running')
-}) 
+})
