a59eceaae83a70dd33f497502d684f3f60b9667b
Muhammad Saad Siddique <26270646+MuhammaadSaad@users.noreply.github.com>
2022-10-16 14:25:20 +0500

    Test

--- Frontend/src/components/cities/CityImage.js
+++ Frontend/src/components/cities/CityImage.js
@@ -4,9 +4,9 @@ const CityImage = ({img, status}) =>{
         return (
             <div className="cities__body__image">
                 <LazyLoadImage src={img} alt={img}/>
-                <div className={status === 'popular' ? 'bestselling'
+                <div className={status === 'Crowded' ? 'bestselling'
                     : status === 'new' ? 'new'
-                        : status === 'Hot' ? 'hot'
+                        : status === 'Less Crowded' ? 'hot'
                             : ''}>
                     {status}
                 </div>
